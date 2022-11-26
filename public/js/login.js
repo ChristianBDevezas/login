d5172e128c58b433e0156777a35f5325dcb3f00c
ChristianBDevezas <christianbdevezas@hotmail.com>
2022-11-26 16:44:31 -0300

    update login.js

--- public/js/login.js
+++ public/js/login.js
@@ -2,12 +2,12 @@ let loginForm = document.querySelector('.login');
 let fieldEmail = document.getElementById('email-login');
 let fieldSenha = document.getElementById('password');
 
-console.log(location.origin);
-console.log(location.pathname);
-console.log(location.pathname.split('/'));
-console.log(location.pathname.split('/').slice(0, 1));
-console.log(location.pathname.split('/')[1]);
-console.log(location.origin + '/' + 'index.html');
+// console.log(location.origin);
+// console.log(location.pathname);
+// console.log(location.pathname.split('/'));
+// console.log(location.pathname.split('/').slice(0, 1));
+// console.log(location.pathname.split('/')[1]);
+// console.log(location.origin + '/' + 'index.html');
 
 const receberRetornoServidor = () => {
     return Promise.resolve({
@@ -16,11 +16,11 @@ const receberRetornoServidor = () => {
 }
 
 const redirecionar = () => {
+    // const path = location.pathname.split('/').slice(0, 1);
     const path = location.pathname.split('/').slice(0, 2);
     path.push('index.html');
     location.href = location.origin + path.join('/');
     // location.href = path.join('/');
-    console.log(location.href);
 
     // const newPage = '/index.html';
     // location.href = location.origin + newPage;
@@ -51,39 +51,3 @@ loginForm.addEventListener('submit', function(event) {
         loginForm.reset();
     }
 });
-
-// let registerForm = document.querySelector('.form-cadastro');
-// let registerInputs = document.querySelectorAll('.form-cadastro input');
-// let registerCheckboxes = document.querySelectorAll('.form-cadastro input[type="checkbox"]');
-// let registerCheckbox1 = document.getElementById('ciente');
-// let registerCheckbox2 = document.getElementById('maiorque18');
-
-// let state = false;
-
-// registerForm.addEventListener('submit', function(event) {
-//     event.preventDefault();
-
-//     registerInputs.forEach(input => {
-//         if(input.type !='checkbox')
-//         if(input.value == '') {
-//             document.querySelector('.err-register-' + input.name).classList.remove('invisible');
-//         } else {
-//             document.querySelector('.err-register-' + input.name).classList.add('invisible');
-//             state = true;
-//         }
-//     });
-
-//     registerCheckboxes.forEach(input => {
-//         if(input.checked == false){
-//             document.querySelector('.err-register-' + input.name).classList.remove('invisible');
-//         } else{
-//             document.querySelector('.err-register-' + input.name).classList.add('invisible');
-//         }
-//     });
-
-//     if(state == true && registerCheckbox1.checked && registerCheckbox2.checked) {
-//         registerForm.reset();
-//         document.querySelector('.mensagem-cadastro').style.display ='block';
-//         setTimeout(() => document.querySelector('.mensagem-cadastro').style.display ='none', 4000);
-//     }
-// });
